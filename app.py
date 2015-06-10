58eaf71ed1af85105591e70adccf3406d459f125
Chad Duffey <chad@dropbox.com>
2015-06-10 14:48:25 +1000

    styling and route for new message

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import (Flask, g, render_template, flash, redirect, url_for)
 from flask.ext.bcrypt import check_password_hash
 from flask.ext.login import (LoginManager, login_user, logout_user, 
-								login_required)
+								login_required, current_user)
 
 import forms
 import models
@@ -29,6 +29,7 @@ def before_request():
 	"""Connect to the database before each request"""
 	g.db = models.DATABASE 
 	g.db.connect()
+	g.user = current_user
 
 @app.after_request
 def after_request(response):
@@ -77,6 +78,18 @@ def logout():
 	logout_user()
 	flash("You have been logged out", "success")
 	return redirect(url_for('index'))
+
+@app.route('/new_post', methods=('GET', 'POST'))
+@login_required
+def post():
+	form = forms.PostForm()
+	if form.validate_on_submit():
+		models.Post.create(user=g.user._get_current_object(),
+			content = form.content.data.strip())
+		flash("Message Posted!", "success")
+		return redirect(url_for('index'))
+	return render_template('post.html', form=form)
+
 
 if __name__ == '__main__':
 	models.initialize()
